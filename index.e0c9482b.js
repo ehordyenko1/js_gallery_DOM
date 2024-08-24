645229d8acb00aaedaa96ca70fdafb9c462940a9
Yehor Gordiienko <egordienko319@gmail.com>
2024-08-24 18:20:30 +0300

    make build

---
+++ index.e0c9482b.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".list-item__link"),r=document.querySelector(".gallery__large-img"),t=!0,l=!1,n=void 0;try{for(var i,a=e[Symbol.iterator]();!(t=(i=a.next()).done);t=!0)!function(){var e=i.value;e.addEventListener("click",function(t){t.preventDefault(),r.src=e.href})}()}catch(e){l=!0,n=e}finally{try{t||null==a.return||a.return()}finally{if(l)throw n}}
+//# sourceMappingURL=index.e0c9482b.js.map
